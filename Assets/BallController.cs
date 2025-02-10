bb0ea789106b8d2a1937bfcb8451731e7d4c02fb
aakash-tir <aakashtirathdas@gmail.com>
2025-02-09 23:12:08 -0800

    created ball and base

---
+++ Assets/BallController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour{
+
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+    private Rigidbody ballRB;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start(){
+        ballRB = GetComponent<Rigidbody>();
+        // Add a listener to the OnSpacePressed event.
+        // When the space key is pressed the
+        // LaunchBall method will be called
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+
+    private void LaunchBall(){
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+
+}
